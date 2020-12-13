4d6a56de867b9c6214ec0266ad3819c3219cc445
Massimo Artizzu <maxart.x@gmail.com>
2020-12-13 11:54:42 +0100

    Day 13

--- day-13/part-2.js
+++ day-13/part-2.js
@@ -1,26 +1,19 @@
 const busList = input.slice(input.indexOf('\n')).split(',');
 const buses = busList.map(Number).filter(Boolean);
 
-// This maps each bus id to its difference in minutes since the first bus in order has departed
-const timetable = busList.reduce((table, id, index) => {
-  if (!isNaN(id)) {
-    table[+id] = index;
-  }
-  return table;
-}, {});
-
 /**
  * How does this works? Iteratively, each bus must depart a multiple of the product
  * of the previous bus.
  * Mathematically speaking, we're trying to solve a bunch of linear Diophantine equations.
  */
-let prod = 1;
-let time = 0;
-for (const id of buses) {
-  let factor = 0;
-  while ((time + factor * prod + timetable[id]) % id !== 0) factor++;
-  time += factor * prod;
-  prod *= id;
-}
+const result = busList.reduce(({ time, prod }, id, diff) => {
+  if (!isNaN(id)) {
+    let factor = 0;
+    while ((time + factor * prod + diff) % id !== 0) factor++;
+    time += factor * prod;
+    prod *= id;
+  }
+  return { time, prod };
+}, { time: 0, prod: 1 });
 
-console.log(time);
+console.log(result.time);
