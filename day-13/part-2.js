13653054cc057bc3e4a7c190cff7afd2174d9e6c
Massimo Artizzu <maxart.x@gmail.com>
2020-12-14 10:01:32 +0100

    Day 13

--- day-13/part-2.js
+++ day-13/part-2.js
@@ -2,14 +2,13 @@ const busList = input.slice(input.indexOf('\n')).split(',');
 
 /**
  * How does this work? Iteratively, each bus must depart a multiple of the product
- * of the previous bus. We find the correct factor, and add to the time passed.
+ * of the previous bus. It should be a multiple of the least common multiple, but all
+ * the id's should be primes (or co-prime).
  * Mathematically speaking, we're trying to solve a bunch of linear Diophantine equations.
  */
 const result = busList.reduce(({ time, prod }, id, diff) => {
   if (!isNaN(id)) {
-    let factor = 0;
-    while ((time + factor * prod + diff) % id !== 0) factor++;
-    time += factor * prod;
+    while (((time += prod) + diff) % id !== 0);
     prod *= id;
   }
   return { time, prod };
